2a85674bc850d12dc9f2205670a9603cb273fee2
MikesSzinyei <szinyeimikes@gmail.com>
2023-06-08 08:32:51 +0200

    First commit

---
+++ hello/src/app.ts
@@ -0,0 +1,30 @@
+var age:number;
+age=28;
+console.log(age);
+
+var fullname:string;
+fullname = 'Nagy József'
+console.log(fullname);
+
+var joido: boolean;
+joido=true;
+console.log(joido);
+
+var nums: number[]=[];
+var a:any;
+a=35;
+a='asd';
+a=false;
+
+var nums:number[]=[];
+nums.push(35);
+nums.push(45);
+nums.push(55);
+console.log(nums[0]);
+
+var names: string[]=[];
+names.push('József');
+
+var numTuple:[number,number,string]=[34,55,'aa'];
+
+
